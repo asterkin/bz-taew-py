846c74a03aa548dd2c9f453be9800ffb4f1d3e68
Asher Sterkin <asher.sterkin@gmail.com>
2025-10-30 18:20:35 +0200

    Resolve #25 (#26)

--- common/scripts/push.sh
+++ common/scripts/push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Undocumented, not sure we should allow it
-git subtree push --prefix=common https://github.com/asterkin/bluezone-app.git $1
+git subtree push --prefix=common https://github.com/asterkin/bz-taew.git $1
